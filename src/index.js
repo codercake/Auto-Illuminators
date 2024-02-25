dcbd853726cf17aba3e885cb80cdeec0433eb32d
codercake <ishithaac2312@gmail.com>
2024-02-25 23:24:46 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { useJsApiLoader } from '@react-google-maps/api'; 
 
 ReactDOM.render(
   <React.StrictMode>
